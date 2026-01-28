044974dd6db06513b241cafbc52b60d8e5131225
krypton1101 <krypton11@mail.ru>
2026-01-28 21:48:40 +0300

    leaderboard entry bg style, fonts not working

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const WS_PORT = process.env.WS_PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/fonts', express.static('fonts'));
 
 // Database setup
 const db = new sqlite3.Database('players.db');
