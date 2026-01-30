a61defeea068da93ada55d1192f31ad434e3ad23
krypton1101 <krypton11@mail.ru>
2026-01-30 21:59:22 +0300

    FONTS!!!

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const WS_PORT = process.env.WS_PORT || 8080;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: "50mb"}));
 app.use(express.static('public'));
 app.use('/fonts', express.static('fonts'));
 
